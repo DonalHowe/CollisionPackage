5dd5e340ecaefd4e0de36d9ae9a64ba5700eb85e
DonalHowe <58332538+DonalHowe@users.noreply.github.com>
2023-05-02 17:40:18 +0100

    [TX-29]
    The unit test for the circle to circle collision

--- CollisionComponent/Packages/ie.setu.collisioncomponent/Tests/collisionComponentTests.cs
+++ CollisionComponent/Packages/ie.setu.collisioncomponent/Tests/collisionComponentTests.cs
@@ -16,8 +16,8 @@ public class collisionComponentTests
 
         colManager.GetComponent<AABB>().VarInitFunc(rightSquare,LefttSquare);
 
-        Collider2D RightCollider = rightSquare.GetComponent<Collider2D>();
-        Collider2D leftCollider = LefttSquare.GetComponent<Collider2D>();
+        BoxCollider2D RightCollider = rightSquare.GetComponent<BoxCollider2D>();
+        BoxCollider2D leftCollider = LefttSquare.GetComponent<BoxCollider2D>();
 
         rightSquare.transform.position = new Vector3(0, 0, 0);
         LefttSquare.transform.position = new Vector3(0, 0, 0);
@@ -123,6 +123,35 @@ public class collisionComponentTests
     }
 
 
-    
+    // circle to circle collision test 
+    [Test]
+    public void CircleToCircleTest()
+    {
+        // creating the objects 
+        GameObject rightCircle = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/circleTwo"));
+        GameObject LeftCircle = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/circleOne"));
+        GameObject colManager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/CollisionManager"));
+
+
+        Rigidbody2D RightmovmentRb = rightCircle.GetComponent<Rigidbody2D>();
+        Rigidbody2D LeftmovmentRb = LeftCircle.GetComponent<Rigidbody2D>();
+
+        // creating the collider
+        CircleCollider2D RightCollider = rightCircle.GetComponent<CircleCollider2D>();
+        CircleCollider2D leftCollider = LeftCircle.GetComponent<CircleCollider2D>();
+
+        colManager.GetComponent<circleTocircle>().VarInitFunc(RightCollider, leftCollider);
+      
+        rightCircle.transform.position = new Vector3(0, 0, 0);
+        LeftCircle.transform.position = new Vector3(0.1f, 0, 0);
+
+        bool status = colManager.GetComponent<circleTocircle>().circleToCircleFunc();
+
+        Assert.IsTrue(status);
+
+
+    }
+
+
 
 }
